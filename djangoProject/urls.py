f448edfe7c3ec7bbec3a6bae948887d688b4c076
Selma Bener <selmaabener@gmail.com>
2024-05-01 19:24:34 +0300

    feat: add general setting model to admin

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("myCV.urls")),
-    path("contact/", include("contact.urls")),
 ]
 
 if settings.DEBUG:
