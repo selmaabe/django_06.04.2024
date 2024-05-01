194d9814d20fb2304b0bf185dd4a7f325af36eca
Selma Bener <selmaabener@gmail.com>
2024-05-01 22:18:56 +0300

    feat: create image setting model

--- myCV/models.py
+++ myCV/models.py
@@ -42,3 +42,45 @@ class GeneralSetting(models.Model):
         verbose_name = 'GeneralSetting'
         verbose_name_plural = 'GeneralSettings'
         ordering = ('name',)
+
+class ImageSetting(models.Model):
+    name = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='Name',
+        help_text='This is variable of the setting.'
+    )
+
+    description = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='Description',
+        help_text=''
+    )
+
+    file = models.ImageField(
+        default='',
+        verbose_name='Image',
+        help_text='',
+        blank=True,
+        upload_to='images/',
+    )
+    updated_date = models.DateTimeField(
+        blank=True,
+        auto_now=True,
+        verbose_name='Updated Date'
+    )
+    created_date = models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+        verbose_name='Created Date'
+    )
+    def __str__(self):
+        return f'ImageSetting: {self.name}'
+
+    class Meta:
+        verbose_name = 'ImageSetting'
+        verbose_name_plural = 'ImageSettings'
+        ordering = ('name',)
