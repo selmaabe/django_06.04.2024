3caeba01354eec2ed805d7e0d599f4463862e44c
Selma Bener <selmaabener@gmail.com>
2024-05-12 16:23:54 +0300

    feat: create experience model

--- myCV/admin.py
+++ myCV/admin.py
@@ -34,3 +34,14 @@ class SkillAdmin(admin.ModelAdmin):
     class Meta:
         model = Skill
 
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'company_name', 'job_title', 'job_location', 'start_date', 'end_date']
+    search_fields = ['company_name', 'job_title', 'job_location']
+    list_editable = ['company_name', 'job_title', 'job_location', 'start_date', 'end_date']
+
+
+
+    class Meta:
+        model = Experience
